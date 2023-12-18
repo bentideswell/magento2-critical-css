3a92e7eb1381d27a0425772c1bed4ea010d53307
Ben Tideswell <ben@fishpig.com>
2023-12-18 15:49:39 +0000

    1.0.2

--- Block/CriticalCss.php
+++ Block/CriticalCss.php
@@ -4,6 +4,8 @@
  */
 namespace FishPig\CriticalCss\Block;
 
+use Magento\Framework\App\View\Deployment\Version\StorageInterface;
+
 class CriticalCss extends \Magento\Framework\View\Element\AbstractBlock
 {
     /**
@@ -19,14 +21,34 @@ class CriticalCss extends \Magento\Framework\View\Element\AbstractBlock
     /**
      *
      */
+    private $cssMinifier = null;
+
+    /**
+     *
+     */
+    private $deploymentVersionStorage = null;
+
+    /**
+     *
+     */
     public function __construct(
         \Magento\Framework\View\Element\Context $context,
         \FishPig\CriticalCss\App\DataProvider $dataProvider,
-        \FishPig\CriticalCss\Model\Config $config
+        \FishPig\CriticalCss\Model\Config $config,
+        \tubalmartin\CssMin\Minifier $cssMinifier,
+        StorageInterface $deploymentVersionStorage,
+        array $data = []
     ) {
         $this->dataProvider = $dataProvider;
         $this->config = $config;
-        parent::__construct($context);
+        $this->cssMinifier = $cssMinifier;
+        $this->deploymentVersionStorage = $deploymentVersionStorage;
+
+        parent::__construct($context, $data);
+
+        $this->setCacheLifetime(
+            $this->getTargetFile() && $this->isCriticalCssEnabled() ? 31449600 : null
+        );
     }
 
     /**
@@ -34,17 +56,50 @@ class CriticalCss extends \Magento\Framework\View\Element\AbstractBlock
      */
     protected function _toHtml()
     {
-        if (!$this->config->isEnabled()) {
+        if (!$this->isCriticalCssEnabled()) {
             return '';
         }
 
-        if ($targetFile = $this->getData('target_file')) {
-            if ($css = $this->dataProvider->getCriticalCss($targetFile)) {
-                return sprintf(
-                    '<style type="text/css">%s</style>',
-                    $css
-                );
-            }
+        if (!($targetFile = $this->getTargetFile())) {
+            return '';
         }
+
+        if (!($css = $this->dataProvider->getCriticalCss($targetFile))) {
+            return '';
+        }
+
+        return sprintf(
+            '<style type="text/css">%s</style>',
+            $this->cssMinifier->run($css)
+        );
+    }
+
+    /**
+     *
+     */
+    public function getTargetFile(): ?string
+    {
+        return $this->getData('target_file') ?: null;
+    }
+
+    /**
+     *
+     */
+    public function isCriticalCssEnabled(): bool
+    {
+        return $this->config->isEnabled();
+    }
+
+    /**
+     *
+     */
+    public function getCacheKeyInfo()
+    {
+        return [
+            $this->getNameInLayout(),
+            $this->getTargetFile(),
+            (int)$this->isCriticalCssEnabled(),
+            $this->deploymentVersionStorage->load()
+        ];
     }
 }
